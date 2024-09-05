ee03941daa608a974830f83ff01fd836fab81de4
hansmalvin <hansmalvin7@gmail.com>
2024-09-05 20:31:04 +0700

    Progress terakhir

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+let iconMenu = document.querySelector('#icon-utk-menu');
+let navBar = document.querySelector('.navbar');
 
-document.querySelector('.download-cv').addEventListener('click', function() {
-    alert('CV Download functionality coming soon!');
-});
-// testing dulu
+iconMenu.onclick = () => {
+    iconMenu.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
+
+
+let navbarScroll = document.querySelectorAll('section')
+let navv = document.querySelectorAll('header nav a')
+
+window.onscroll = () =>{
+    navbarScroll.forEach(tes => {
+        let top = window.screenY;
+        let secoff = tes.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top <offset + height) {
+            navv.forEach(lin => {
+                lin.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            })
+        }
+    })
+}
+
+// harus dibawha
+window.onscroll = () => {
+    let judul = document.querySelector('header')
+
+    judul.classList.toggle('scroll', window.scrollY > 100)
+
+    iconMenu.classList.remove('bx-x');
+    navBar.classList.remove('active');
+}
+
+
